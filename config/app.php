7cc06e0a9005d810d09237840f1c830fcf0710ba
Sanjiv Shrestha <sanjeev.shrestha999@gmail.com>
2020-05-08 14:14:35 +0545

    NewsAPI added and data retrieved

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        NewsAPI\NewsAPIServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -211,6 +212,7 @@ return [
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
+        'NewsAPI' => NewsAPI\Facades\NewsAPI::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
